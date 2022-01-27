70906e8901f1e96c498f64ab63ecf4b401afb6e6
Piotr Ulanowski <46483137+PUlanowski@users.noreply.github.com>
2022-01-27 20:41:21 +0100

    Delete IaC.sh

--- IaC.sh
+++
@@ -1,2 +0,0 @@
-gcloud dataflow jobs run  pubsub-to-gcs2 --gcs-location gs://dataflow-templates-us-central1/latest/Cloud_PubSub_to_GCS_Text --region us-central1 --staging-location gs://dataflow-temp-piotr-ulanowski-sandbox-1/temp/ --parameters inputTopic=projects/piotr-ulanowski-sandbox-1/topics/twitt_stream,outputDirectory=gs://dataflow-temp-piotr-ulanowski-sandbox-1/tweets/,outputFilenamePrefix=tweet-
-
